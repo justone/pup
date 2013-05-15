97792f9f517f7d950765da7f5174d896e4055a74
Nate Jones <nate@endot.org>
2013-05-15 12:39:42 -0400

    set shell to bash

--- site/server/manifests/users/nate.pp
+++ site/server/manifests/users/nate.pp
@@ -1,7 +1,8 @@
 class server::users::nate {
     user { 'nate':
       managehome => true,
-      ensure => present,
-      comment => 'Nate Jones',
+      ensure     => present,
+      comment    => 'Nate Jones',
+      shell      => '/bin/bash'
     }
 }
