af0daf15757f589fc3e24bab92b7920126b3a79b
Nate Jones <nate@endot.org>
2014-05-11 23:14:12 -0400

    modernize usage of puppetlabs firewall

--- main.pp
+++ main.pp
@@ -1,34 +1,16 @@
-# Always persist firewall rules
-file { '/etc/iptables':
-  ensure => directory,
-} ->
-exec { 'persist-firewall':
-  command     => $operatingsystem ? {
-    /(Ubuntu|Debian)/          => '/sbin/iptables-save > /etc/iptables/rules.v4',
-    /(RedHat|CentOS)/ => '/sbin/iptables-save > /etc/sysconfig/iptables',
-  },
-  refreshonly => true,
-}
-
-# These defaults ensure that the persistence command is executed after 
-# every change to the firewall, and that pre & post classes are run in the
-# right order to avoid potentially locking you out of your box during the
-# first puppet run.
+# Ensure that the firewall pre and post are run at the right times, to avoid
+# lock out
 Firewall {
-  notify  => Exec['persist-firewall'],
   before  => Class['server::firewall::post'],
   require => Class['server::firewall::pre'],
 }
-Firewallchain {
-  notify  => Exec['persist-firewall'],
-}
 
-# Purge unmanaged firewall resources
-#
-# This will clear any existing rules, and make sure that only rules
-# defined in puppet exist on the machine
+# Don't purge resources, this allows docker to coexist
 resources { "firewall":
   purge => false
 }
+
+# And, of course, include basic firewall support
+class { 'firewall': }
 
 include server
